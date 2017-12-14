8b696378f38742581b91d888c5016b49b5540fc0
Emelie Sidesiö <esidesio@gmail.com>
2017-12-14 13:47:52 +0100

    grid layout

--- src/components/app.js
+++ src/components/app.js
@@ -4,8 +4,22 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>
-        Find me in src/app.js!
+      <div className="grid-container">
+        <div className="wide">
+          <span>Helvetica</span>
+          <span>Designed by</span>
+          <span>Max Miedinger 1957</span>
+        </div>
+        <div className="tall wide">2</div>
+        <div className="tall">3</div>
+        <div>4</div>
+        <div>5</div>
+        <div>6</div>
+        <div>7</div>
+        <div>8</div>
+        <div className="wide">9</div>
+        <div className="wide">10</div>
+        <div>11</div>
       </div>
     )
   }
