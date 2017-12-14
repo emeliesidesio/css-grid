ec3e12a8c9254b48152380156c468daeeedde158
Emelie Sidesiö <esidesio@gmail.com>
2017-12-14 15:02:59 +0100

    made stuff happen

--- src/components/app.js
+++ src/components/app.js
@@ -1,4 +1,7 @@
 import React from "react"
+import a from "assets/a.png"
+import curly from "assets/curly_bracet.png"
+import and from "assets/and.png"
 
 class App extends React.Component {
 
@@ -6,20 +9,48 @@ class App extends React.Component {
     return (
       <div className="grid-container">
         <div className="wide">
-          <span>Helvetica</span>
-          <span>Designed by</span>
-          <span>Max Miedinger 1957</span>
+          <span className="logo">Helvetica</span>
+          <span className="byline">Designed by</span>
+          <span className="bodytext">Max Miedinger 1957</span>
         </div>
-        <div className="tall wide">2</div>
-        <div className="tall">3</div>
-        <div>4</div>
-        <div>5</div>
-        <div>6</div>
-        <div>7</div>
-        <div>8</div>
-        <div className="wide">9</div>
-        <div className="wide">10</div>
-        <div>11</div>
+        <div className="tall wide">
+          <img className="image-a" src={a} alt="a" />
+        </div>
+        <div className="tall">
+          <img className="image-curly" src={curly} alt="curly" />
+        </div>
+        <div>
+          <img src={and} alt="and" />
+        </div>
+        <div>
+          <span className="headline">Usage</span>
+          <span className="bodytext">/ Apple’s iOS interface / NYC’s MTA & Chicago's Transit Authority signage / NASA Space Shuttle orbiter</span>
+        </div>
+        <div>
+          <span className="headline">Widths & weights</span>
+          <span className="bodytext">/ 9 weights / three widths (normal, condensed, extended) / 51 fonts total</span>
+        </div>
+        <div>
+          <span className="headline">Strokes</span>
+          <span className="bodytext">/ cut horizontally or vertically / distinguishes Helvetica from dupes like Arial</span>
+        </div>
+        <div>
+          <span className="headline">Punctuation</span>
+          <span className="bodytext">/heavier punctuation marks than Helvetica</span>
+        </div>
+        <div className="wide">
+          <span className="quote">“I don’t care what it says, as long as it’s in Helvetica“</span>
+        </div>
+        <div className="wide">
+          <span className="alphabet">abcdefghijk
+            lmnopqrstu
+            vwxyz
+          </span>
+        </div>
+        <div>
+          <span className="headline">Helvetica – the movie</span>
+          <span className="bodytext">/ documentary by Gary Hustwit / released for Helvetica's 50th birthday (2007)</span>
+        </div>
       </div>
     )
   }
